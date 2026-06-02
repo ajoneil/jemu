005f27a36c3e025d31f7d8cdba29d3006c43f9bf
ArkoSammy12 <jamesvillarreal12@gmail.com>
2026-06-02 02:55:00 -0500

    RCA Studio II: Update keypad action labels

--- core/src/main/java/io/github/arkosammy12/jemu/core/rca/studioii/RCAStudioIIKeypad.java
+++ core/src/main/java/io/github/arkosammy12/jemu/core/rca/studioii/RCAStudioIIKeypad.java
@@ -61,27 +61,27 @@ public class RCAStudioIIKeypad<E extends RCAStudioIIEmulator> extends SystemCont
     }
 
     public enum Actions implements Action {
-        KEYPAD1_0("0", 0x0, Keypad.KEYPAD_1),
-        KEYPAD1_1("1", 0x1, Keypad.KEYPAD_1),
-        KEYPAD1_2("2", 0x2, Keypad.KEYPAD_1),
-        KEYPAD1_3("3", 0x3, Keypad.KEYPAD_1),
-        KEYPAD1_4("4", 0x4, Keypad.KEYPAD_1),
-        KEYPAD1_5("5", 0x5, Keypad.KEYPAD_1),
-        KEYPAD1_6("6", 0x6, Keypad.KEYPAD_1),
-        KEYPAD1_7("7", 0x7, Keypad.KEYPAD_1),
-        KEYPAD1_8("8", 0x8, Keypad.KEYPAD_1),
-        KEYPAD1_9("9", 0x9, Keypad.KEYPAD_1),
+        KEYPAD1_0("Keypad 1 key 0", 0x0, Keypad.KEYPAD_1),
+        KEYPAD1_1("Keypad 1 key 1", 0x1, Keypad.KEYPAD_1),
+        KEYPAD1_2("Keypad 1 key 2", 0x2, Keypad.KEYPAD_1),
+        KEYPAD1_3("Keypad 1 key 3", 0x3, Keypad.KEYPAD_1),
+        KEYPAD1_4("Keypad 1 key 4", 0x4, Keypad.KEYPAD_1),
+        KEYPAD1_5("Keypad 1 key 5", 0x5, Keypad.KEYPAD_1),
+        KEYPAD1_6("Keypad 1 key 6", 0x6, Keypad.KEYPAD_1),
+        KEYPAD1_7("Keypad 1 key 7", 0x7, Keypad.KEYPAD_1),
+        KEYPAD1_8("Keypad 1 key 8", 0x8, Keypad.KEYPAD_1),
+        KEYPAD1_9("Keypad 1 key 9", 0x9, Keypad.KEYPAD_1),
 
-        KEYPAD2_0("0", 0x0, Keypad.KEYPAD_2),
-        KEYPAD2_1("1", 0x1, Keypad.KEYPAD_2),
-        KEYPAD2_2("2", 0x2, Keypad.KEYPAD_2),
-        KEYPAD2_3("3", 0x3, Keypad.KEYPAD_2),
-        KEYPAD2_4("4", 0x4, Keypad.KEYPAD_2),
-        KEYPAD2_5("5", 0x5, Keypad.KEYPAD_2),
-        KEYPAD2_6("6", 0x6, Keypad.KEYPAD_2),
-        KEYPAD2_7("7", 0x7, Keypad.KEYPAD_2),
-        KEYPAD2_8("8", 0x8, Keypad.KEYPAD_2),
-        KEYPAD2_9("9", 0x9, Keypad.KEYPAD_2);
+        KEYPAD2_0("Keypad 2 key 0", 0x0, Keypad.KEYPAD_2),
+        KEYPAD2_1("Keypad 2 key 1", 0x1, Keypad.KEYPAD_2),
+        KEYPAD2_2("Keypad 2 key 2", 0x2, Keypad.KEYPAD_2),
+        KEYPAD2_3("Keypad 2 key 3", 0x3, Keypad.KEYPAD_2),
+        KEYPAD2_4("Keypad 2 key 4", 0x4, Keypad.KEYPAD_2),
+        KEYPAD2_5("Keypad 2 key 5", 0x5, Keypad.KEYPAD_2),
+        KEYPAD2_6("Keypad 2 key 6", 0x6, Keypad.KEYPAD_2),
+        KEYPAD2_7("Keypad 2 key 7", 0x7, Keypad.KEYPAD_2),
+        KEYPAD2_8("Keypad 2 key 8", 0x8, Keypad.KEYPAD_2),
+        KEYPAD2_9("Keypad 2 key 9", 0x9, Keypad.KEYPAD_2);
 
         private final String label;
         private final Keypad keypad;
