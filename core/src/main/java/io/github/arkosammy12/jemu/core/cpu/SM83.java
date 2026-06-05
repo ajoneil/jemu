4fe52e210028a7ce082645f21ba7736eac7671dc
ArkoSammy12 <jamesvillarreal12@gmail.com>
2026-06-05 13:51:23 -0500

    SM83: Trigger halt bug regardless of IME status

--- core/src/main/java/io/github/arkosammy12/jemu/core/cpu/SM83.java
+++ core/src/main/java/io/github/arkosammy12/jemu/core/cpu/SM83.java
@@ -843,9 +843,7 @@ public class SM83<S extends SM83.SystemBus> implements Processor {
                     switch (machineCycleIndex) {
                         case 0 -> {
                             if (interruptsPending()) {
-                                if (!getIME()) {
-                                    this.haltBug = true;
-                                }
+                                this.haltBug = true;
                                 machineCycleIndex = TERMINATE_INSTRUCTION;
                             } else {
                                 this.mode = Mode.HALTED;
